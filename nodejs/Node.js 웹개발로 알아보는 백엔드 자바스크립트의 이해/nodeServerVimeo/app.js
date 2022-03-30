b40c41e855cba288708b4ff04104ffa9a922e294
DESKTOP-4OLJ2SJ\hie09 <hie0908@naver.com>
2022-03-30 20:57:53 +0900

    Request, Response 처리 기본

--- nodejs/Node.js 웹개발로 알아보는 백엔드 자바스크립트의 이해/nodeServerVimeo/app.js
+++ nodejs/Node.js 웹개발로 알아보는 백엔드 자바스크립트의 이해/nodeServerVimeo/app.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var app = express()
+var bodyParser = require('body-parser')
 
 app.listen(3000, function() {
 	console.log("start! express server on port 3000");
 });
 
 app.use(express.static('public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true})) // json이 아닌 형태일 때 urlencoded를 사용함
+app.set('view engine', 'ejs')
 
 // url routing
 app.get('/', function(req, res) {
@@ -16,4 +20,17 @@ app.get('/', function(req, res) {
 
 app.get('/main', function(req, res) {
 	res.sendFile(__dirname + '/public/main.html')
+})
+
+app.post('/email_post', function(req,res){
+	// get : req.param('email')
+	console.log(req.body.email)
+	//res.send("<h1>Welcome !"+req.body.email+"</h1>")
+	res.render('email.ejs', {'email':req.body.email})
+})
+
+app.post('/ajax_email_send', function(req, res){
+	console.log(req.body.email)
+	var responseData = {'result':'ok', 'email':req.body.email}
+	res.json(responseData)
 })
