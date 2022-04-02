f5c30fc8c802132ac596556343a2c2c64f0ea451
DESKTOP-4OLJ2SJ\hie09 <hie0908@naver.com>
2022-04-02 23:08:36 +0900

    DB에 데이터 추가

--- nodejs/Node.js 웹개발로 알아보는 백엔드 자바스크립트의 이해/nodeServerVimeo/router/index.js
+++ nodejs/Node.js 웹개발로 알아보는 백엔드 자바스크립트의 이해/nodeServerVimeo/router/index.js
@@ -4,15 +4,16 @@ var router = express.Router();
 var path = require('path')
 var main = require('./main/main')
 var email = require('./email/email')
+var email = require('./join/index')
+
 
 // url routing
 router.get('/', function(req, res) {
-	console.log('test');
-	// res.send("<h1>hi friend</h1>")
 	res.sendFile(path.join(__dirname + '../public/main.html'))
 });
 
 router.use('/main', main)
 router.use('/email', email)
+router.use('/join', join)
 
 module.exports = router;
