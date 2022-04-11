b5dd9662afc914b2d602d561035f88e83163e67b
DESKTOP-4OLJ2SJ\hie09 <hie0908@naver.com>
2022-04-11 21:35:59 +0900

    로그아웃 처리

--- nodejs/Node.js 웹개발로 알아보는 백엔드 자바스크립트의 이해/nodeServerVimeo/router/main/main.js
+++ nodejs/Node.js 웹개발로 알아보는 백엔드 자바스크립트의 이해/nodeServerVimeo/router/main/main.js
@@ -3,12 +3,13 @@ var app = express()
 var router = express.Router();
 var path = require('path')
 
-
+// main page는 login인 될 때만(즉 세션정보가 있을 때만) 접근이 가능하다.
 router.get('/', function(req, res) {
 	console.log('main js loaded', req.user)
 	var id = req.user;
 	//res.sendFile(path.join(__dirname, '../../public/main.html'))
-	res.render('main.ejs', {'id' : id})
+	if(!id) res.render('login.ejs');
+	res.render('main.ejs', {'id' : id});
 });
 
 module.exports = router;
