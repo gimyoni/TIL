393e6fba3b8199298379706a5c7e708d192f8be0
YeonHui Kim <hie0908@naver.com>
2022-05-11 21:38:52 +0900

    JS > 멀리 뛰기

---
+++ Algorithm/Programmers/Lv3/멀리 뛰기.js
@@ -0,0 +1,8 @@
+function solution(n) {
+    var answer = 0;
+    const dp = Array(n+1).fill(1)
+    for(let i = 2; i <= n; i++){
+        dp[i] = (dp[i-1] + dp[i-2]) % 1234567
+    }
+    return dp[n];
+}
