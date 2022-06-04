46653b4eed1f2fc5e935e8f7d21a390d43b01eca
YeonHui Kim <hie0908@naver.com>
2022-06-04 23:34:36 +0900

    JS > 나누어 떨어지는 숫자 배열

---
+++ Algorithm/Programmers/Lv1/나누어 떨어지는 숫자 배열.js
@@ -0,0 +1,13 @@
+function solution(arr, divisor) {
+    let answer = [];
+    for(let a of arr){
+        if(a%divisor===0){
+            answer.push(a);
+        }
+    }
+    if(Array.isArray(answer)&&answer.length===0){
+        answer.push(-1);
+    }
+    answer.sort((a,b)=>a-b);
+    return answer;
+}
