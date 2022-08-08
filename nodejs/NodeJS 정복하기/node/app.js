a0ae1cd6fe3b891014f0e22b641b0e85b4da2f01
DESKTOP-4OLJ2SJ\hie09 <hie0908@naver.com>
2022-08-08 23:11:05 +0900

    NodeJS 6강 - Core Module (readline)

--- nodejs/NodeJS 정복하기/node/app.js
+++ nodejs/NodeJS 정복하기/node/app.js
@@ -1,9 +1,24 @@
-const add = require('./add');
-const mult = require('./mult');
-const stat = require('./stat');
+// 05
 
-console.log(stat.pi);
-console.log(stat.mean([1, 2, 3, 4, 5]));
+// const add = require('./add');
+// const mult = require('./mult');
+// const stat = require('./stat');
 
-const result = mult(5, 10);
-console.log(result);
+// console.log(stat.pi);
+// console.log(stat.mean([1, 2, 3, 4, 5]));
+
+// const result = mult(5, 10);
+// console.log(result);
+
+// 06
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.question("what is your name ? ", (answer)=>{
+    console.log("Hello "+answer);
+    
+    rl.close();
+})
