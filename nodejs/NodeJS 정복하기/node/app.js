57b23f74fb1d6e98cfbdee96e4505e4ad74a2dda
DESKTOP-4OLJ2SJ\hie09 <hie0908@naver.com>
2022-09-09 23:56:52 +0900

    NodeJS 21강 - Post Request와 Body Parser 사용

--- nodejs/NodeJS 정복하기/node/app.js
+++ nodejs/NodeJS 정복하기/node/app.js
@@ -98,7 +98,7 @@
 
 const express = require("express");
 const hbs = require("express-handlebars");
-
+const bodyParser = require("body-parser");
 const server = express();
 const words = require("./db/words.json");
 
@@ -117,11 +117,19 @@ server.engine(
 );
 server.set("view engine", "hbs");
 server.use(express.static(__dirname+"/public"));
-
+server.use(bodyParser.urlencoded({ extended: false }));
 server.get("/", (req, res) => {
-    res.render("home", {
-      words,
-    });
+  res.render("home", {
+    words,
+  });
+});
+server.post("/", (req, res) => {
+  const { query } = req.body;
+  res.render("home", {
+    words: words.filter((w) =>
+      w.word.toLowerCase().includes(query.toLowerCase())
+    ),
+  });
 });
   
 server.get("/add", (req, res) => {
