fc7fe06e8fea11aa3009b7f7d8de4ef33f2c0584
YeonHui Kim <hie0908@naver.com>
2022-11-13 23:34:01 +0900

    완전탐색 > 모음사전

---
+++ Algorithm/Programmers/완전탐색/모음사전.py
@@ -0,0 +1,11 @@
+from itertools import product
+
+def solution(word):
+    answer = []
+    li = ['A', 'E', 'I', 'O', 'U']
+    for i in range(1, 6):
+        for p in product(li, repeat = i):
+            answer.append(''.join(list(p)))
+
+    answer.sort()
+    return answer.index(word) + 1
